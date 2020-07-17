e3dfd3d930486e47a675ad3021ba7db2730df612
DrTraxx <66879705+DrTraxx@users.noreply.github.com>
2020-07-17 09:29:27 +0200

    Update allianceHeader.user.js

--- allianceHeader.user.js
+++ allianceHeader.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Alliance-Header
-// @version      1.0.0
+// @version      1.1.0
 // @author       DrTraxx
 // @include      *://www.leitstellenspiel.de/*
 // @include      *://leitstellenspiel.de/*
@@ -13,7 +13,8 @@
 
     var candidatureCount = $('#alliance_candidature_count').text().replace('(','').replace(')','');
 
-    $('#menu_alliance').parent().after(`<li><a id="schoolings" class="lightbox-open" href="/schoolings" ><div class="glyphicon glyphicon-education"></div></a></li`);
+    $('#menu_alliance').parent().after(`<li><a id="rules" class="lightbox-open" href="/verband/regeln/${alliance_id}" ><div class="glyphicon glyphicon-book"></div></a></li`);
+    $('#rules').parent().after(`<li><a id="schoolings" class="lightbox-open" href="/schoolings" ><div class="glyphicon glyphicon-education"></div></a></li`);
     $('#schoolings').parent().after(`<li><a id="alliance_messages_header" class="lightbox-open" href="/alliance_messages" ><div class="glyphicon glyphicon-envelope"></div></a></li>`);
     $('#alliance_messages_header').parent().after(`<li><a id="alliance_forum_header" class="lightbox-open" href="/alliance_threads" ><div class="glyphicon glyphicon-list-alt"></div></a></li>`);
     if(alliance_admin || alliance_coadmin){
