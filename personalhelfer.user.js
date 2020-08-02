cbf88cbc8bb29607e4d6635ba9fbe3e5bcb80b88
DrTraxx <66879705+DrTraxx@users.noreply.github.com>
2020-08-02 18:30:49 +0200

    Update personalhelfer.user.js

--- personalhelfer.user.js
+++ personalhelfer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Personalhelfer
-// @version      1.0.1
+// @version      1.1.0
 // @description  Werbephasen und Personalsoll in der Gebaeudeuebersicht auswaehlen
 // @author       DrTraxx
 // @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de|missionchief\.co\.uk|missionchief\.com|meldkamerspel\.com|centro-de-mando\.es|missionchief-australia\.com|larmcentralen-spelet\.se|operatorratunkowy\.pl|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|jogo-operador112\.com|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com)\/buildings\/.*\
@@ -20,6 +20,7 @@
     var hireStart = `<div class="alert fade in alert-success "><button class="close" data-dismiss="alert" type="button">×</button>Die Einstellungsphase wurde gestartet.</div>`;
     var hireEnd = `<div class="alert fade in alert-success "><button class="close" data-dismiss="alert" type="button">×</button>Die Einstellungsphase wurde beendet.</div>`;
     var building = aBuildings.filter((obj) => obj.id == buildingId)[0];
+    var maxPersonal = $('dl[class*="dl-horizontal"]').children('dd')[2].innerText.replace(/[a-zA-Z]./g,'').replace(',','').trim().split(':')[1] ? $('dl[class*="dl-horizontal"]').children('dd')[2].innerText.replace(/[a-zA-Z]./g,'').replace(',','').trim().split(':')[1].trim() : 0;
     var noPersonalBuildings = [1,3,4,7,8,10,14];
     var hire = false;
     var cssHide = {"display":"none"};
@@ -40,7 +41,7 @@
                        <a id="hire_do_3" class="btn btn-default" style="display:${!hire ? `` : `none`}">3 Tage werben</a>
                        <a id="hire_do_0" class="btn btn-danger" style="display:${hire ? `` : `none`}">Einstellungsphase abbrechen</a>
                        <a id="hire_do_automatic" class="btn btn-default" style="display:${user_premium && !hire ? `` : `none`}">automatisch</a>
-                       <input class="numeric integer optional form-control" type="number" placeholder="Soll" id="setPersonal" style="width:6em">
+                       <input class="numeric integer optional form-control" type="number" value="${maxPersonal}" id="setPersonal" style="width:5em">
                        <a id="savePersonal" class="btn btn-success">Speichern</a>
                      </div>`);
     });
