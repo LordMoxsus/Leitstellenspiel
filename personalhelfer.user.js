a2000617944aca9098741fc8d2d71ba52bb2ec1f
DrTraxx <66879705+DrTraxx@users.noreply.github.com>
2020-08-09 08:53:51 +0200

    Update personalhelfer.user.js

--- personalhelfer.user.js
+++ personalhelfer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Personalhelfer
-// @version      1.2.0
+// @version      1.3.0
 // @description  Werbephasen und Personalsoll in der Gebaeudeuebersicht auswaehlen
 // @author       DrTraxx
 // @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de|missionchief\.co\.uk|missionchief\.com|meldkamerspel\.com|centro-de-mando\.es|missionchief-australia\.com|larmcentralen-spelet\.se|operatorratunkowy\.pl|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|jogo-operador112\.com|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com)\/buildings\/.*\
@@ -26,16 +26,16 @@
             hire = true;
         }
     }).done(() =>{
-        $('dl[class*="dl-horizontal"]')
-            .after(`<div class="btn-group input-group">
-                       <a id="hire_do_1" class="btn btn-default" style="display:${!hire ? `` : `none`}">1 Tag werben</a>
-                       <a id="hire_do_2" class="btn btn-default" style="display:${!hire ? `` : `none`}">2 Tage werben</a>
-                       <a id="hire_do_3" class="btn btn-default" style="display:${!hire ? `` : `none`}">3 Tage werben</a>
-                       <a id="hire_do_0" class="btn btn-danger" style="display:${hire ? `` : `none`}">Einstellungsphase abbrechen</a>
-                       <a id="hire_do_automatic" class="btn btn-default" style="display:${user_premium && !hire ? `` : `none`}">automatisch</a>
-                       <input class="numeric integer optional form-control" type="number" value="${maxPersonal}" id="setPersonal" style="width:5em">
-                       <a id="savePersonal" class="btn btn-success">Speichern</a>
-                     </div>`);
+        $('div[class="btn-group pull-right"]')
+            .addClass('input-group')
+            .append(`<br>
+                     <a id="hire_do_1" class="btn btn-default btn-xs" style="display:${!hire ? `` : `none`}">1 Tag werben</a>
+                     <a id="hire_do_2" class="btn btn-default btn-xs" style="display:${!hire ? `` : `none`}">2 Tage werben</a>
+                     <a id="hire_do_3" class="btn btn-default btn-xs" style="display:${!hire ? `` : `none`}">3 Tage werben</a>
+                     <a id="hire_do_0" class="btn btn-danger btn-xs" style="display:${hire ? `` : `none`}">Einstellungsphase abbrechen</a>
+                     <a id="hire_do_automatic" class="btn btn-default btn-xs" style="display:${user_premium && !hire ? `` : `none`}">automatisch</a>
+                     <input class="numeric integer optional form-control" type="number" value="${maxPersonal}" id="setPersonal" style="width:5em;height:22px">
+                     <a id="savePersonal" class="btn btn-success btn-xs">Speichern</a>`);
     });
 
     $("body").on("click", "#hire_do_1",function(){
