400389b3d7e6aac55bce07fba77264c9103b0990
DrTraxx <66879705+DrTraxx@users.noreply.github.com>
2020-08-12 23:17:04 +0200

    Update personalhelfer.user.js

--- personalhelfer.user.js
+++ personalhelfer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Personalhelfer
-// @version      1.3.0
+// @version      1.3.1
 // @description  Werbephasen und Personalsoll in der Gebaeudeuebersicht auswaehlen
 // @author       DrTraxx
 // @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de|missionchief\.co\.uk|missionchief\.com|meldkamerspel\.com|centro-de-mando\.es|missionchief-australia\.com|larmcentralen-spelet\.se|operatorratunkowy\.pl|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|jogo-operador112\.com|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com)\/buildings\/.*\
@@ -26,16 +26,17 @@
             hire = true;
         }
     }).done(() =>{
-        $('div[class="btn-group pull-right"]')
-            .addClass('input-group')
-            .append(`<br>
-                     <a id="hire_do_1" class="btn btn-default btn-xs" style="display:${!hire ? `` : `none`}">1 Tag werben</a>
-                     <a id="hire_do_2" class="btn btn-default btn-xs" style="display:${!hire ? `` : `none`}">2 Tage werben</a>
-                     <a id="hire_do_3" class="btn btn-default btn-xs" style="display:${!hire ? `` : `none`}">3 Tage werben</a>
-                     <a id="hire_do_0" class="btn btn-danger btn-xs" style="display:${hire ? `` : `none`}">Einstellungsphase abbrechen</a>
-                     <a id="hire_do_automatic" class="btn btn-default btn-xs" style="display:${user_premium && !hire ? `` : `none`}">automatisch</a>
+        $('ol[class="breadcrumb"]')
+            //.parent()
+            .append(`<div class="btn-group input-group pull-right" style="float:right">
+                     <a id="hire_do_1" class="btn btn-default btn-xs" style="display:${!hire ? `inline` : `none`}">1 Tag werben</a>
+                     <a id="hire_do_2" class="btn btn-default btn-xs" style="display:${!hire ? `inline` : `none`}">2 Tage werben</a>
+                     <a id="hire_do_3" class="btn btn-default btn-xs" style="display:${!hire ? `inline` : `none`}">3 Tage werben</a>
+                     <a id="hire_do_0" class="btn btn-danger btn-xs" style="display:${hire ? `inline` : `none`}">Einstellungsphase abbrechen</a>
+                     <a id="hire_do_automatic" class="btn btn-default btn-xs" style="display:${user_premium && !hire ? `inline` : `none`}">automatisch</a>
                      <input class="numeric integer optional form-control" type="number" value="${maxPersonal}" id="setPersonal" style="width:5em;height:22px">
-                     <a id="savePersonal" class="btn btn-success btn-xs">Speichern</a>`);
+                     <a id="savePersonal" class="btn btn-success btn-xs">Speichern</a>
+                    </div>`);
     });
 
     $("body").on("click", "#hire_do_1",function(){
