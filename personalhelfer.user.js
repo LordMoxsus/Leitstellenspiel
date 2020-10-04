de5a85e960665f3526fddc6228920da48efb3d77
Patrick <66879705+DrTraxx@users.noreply.github.com>
2020-10-04 11:07:56 +0200

    bugfix

--- personalhelfer.user.js
+++ personalhelfer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Personalhelfer
-// @version      1.4.1
+// @version      1.4.2
 // @description  Werbephasen und Personalsoll in der Gebaeudeuebersicht auswaehlen
 // @author       DrTraxx
 // @include      /^https?:\/\/[www.]*(?:leitstellenspiel\.de|missionchief\.co\.uk|missionchief\.com|meldkamerspel\.com|centro-de-mando\.es|missionchief-australia\.com|larmcentralen-spelet\.se|operatorratunkowy\.pl|operatore112\.it|operateur112\.fr|dispetcher112\.ru|alarmcentral-spil\.dk|nodsentralspillet\.com|operacni-stredisko\.cz|112-merkez\.com|jogo-operador112\.com|operador193\.com|centro-de-mando\.mx|dyspetcher101-game\.com|missionchief-japan\.com)\/buildings\/.*\
@@ -17,25 +17,21 @@
     var hire = false;
     var cssHide = {"display":"none"};
     var cssShow = {"display":"inline"};
-    var building = {};
+    var building = await $.getJSON("/api/buildings/" + buildingId, (data) => data);
     var noPersonalBuildings = [1,3,4,7,8,10,14];
 
-    $.getJSON("/api/buildings/" + buildingId, function(data) {
-        building = data;
-        if(building.hiring_automatic === true || building.hiring_phase > 0) hire = true;
-    }).done(function() {
-        if(noPersonalBuildings.includes(building.building_type)) return false;
-        $(".breadcrumb")
-            .append(`<div class="btn-group input-group pull-right" style="float:right">
-                     <a id="hire_do_1" class="btn btn-default btn-xs" style="display:${!hire ? `inline` : `none`}">1 Tag werben</a>
-                     <a id="hire_do_2" class="btn btn-default btn-xs" style="display:${!hire ? `inline` : `none`}">2 Tage werben</a>
-                     <a id="hire_do_3" class="btn btn-default btn-xs" style="display:${!hire ? `inline` : `none`}">3 Tage werben</a>
-                     <a id="hire_do_0" class="btn btn-danger btn-xs" style="display:${hire ? `inline` : `none`}">Einstellungsphase abbrechen</a>
-                     <a id="hire_do_automatic" class="btn btn-default btn-xs" style="display:${user_premium && !hire ? `inline` : `none`}">automatisch</a>
-                     <input class="numeric integer optional form-control" type="number" value="${building.personal_count_target}" id="setPersonal" style="width:5em;height:22px">
-                     <a id="savePersonal" class="btn btn-success btn-xs">Speichern</a>
-                     </div>`);
-    });
+    if(building.hiring_automatic === true || building.hiring_phase > 0) hire = true;
+    if(noPersonalBuildings.includes(building.building_type)) return false;
+    $(".breadcrumb")
+        .append(`<div class="btn-group input-group pull-right" style="float:right">
+                   <a id="hire_do_1" class="btn btn-default btn-xs" style="display:${!hire ? `inline` : `none`}">1 Tag werben</a>
+                   <a id="hire_do_2" class="btn btn-default btn-xs" style="display:${!hire ? `inline` : `none`}">2 Tage werben</a>
+                   <a id="hire_do_3" class="btn btn-default btn-xs" style="display:${!hire ? `inline` : `none`}">3 Tage werben</a>
+                   <a id="hire_do_0" class="btn btn-danger btn-xs" style="display:${hire ? `inline` : `none`}">Einstellungsphase abbrechen</a>
+                   <a id="hire_do_automatic" class="btn btn-default btn-xs" style="display:${user_premium && !hire ? `inline` : `none`}">automatisch</a>
+                   <input class="numeric integer optional form-control" type="number" value="${building.personal_count_target}" id="setPersonal" style="width:5em;height:22px">
+                   <a id="savePersonal" class="btn btn-success btn-xs">Speichern</a>
+                 </div>`);
 
     $("body").on("click", "#hire_do_1",function(){
         $.get(`/buildings/${buildingId}/hire_do/1`).done(function() {
