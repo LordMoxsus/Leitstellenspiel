cc3ca569964300307f4a81bd20e7d184986d6def
Patrick <66879705+DrTraxx@users.noreply.github.com>
2020-10-18 18:20:46 +0200

    created firstresponder_uk

--- firsresponder_uk.user.js
+++ firsresponder_uk.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         FirstResponder (Original by JuMaHo)
-// @version      1.0.2
+// @name         FirstResponder (Original by JuMaHo) - Version UK
+// @version      1.0.0
 // @description  wählt das nächstgelegene FirstResponder-Fahrzeug aus
 // @author       DrTraxx
-// @match        *://www.leitstellenspiel.de/missions/*
-// @match        *://www.leitstellenspiel.de/aaos/*/edit
+// @match        *://www.missionchief.co.uk/missions/*
+// @match        *://www.missionchief.co.uk/aaos/*/edit
 // @grant        none
 // ==/UserScript==
 /* global $ */
@@ -12,85 +12,85 @@
 (async function() {
     'use strict';
 
-    if(!localStorage.aVehicleTypesNew || JSON.parse(localStorage.aVehicleTypesNew).lastUpdate < (new Date().getTime() - 5 * 1000 * 60)) {
-        await $.getJSON("https://drtraxx.github.io/vehicletypes.json").done(data => localStorage.setItem('aVehicleTypesNew', JSON.stringify({lastUpdate: new Date().getTime(), value: data})) );
+    if(!localStorage.aVehicleTypesUk || JSON.parse(localStorage.aVehicleTypesUk).lastUpdate < (new Date().getTime() - 5 * 1000 * 60)) {
+        await $.getJSON("https://lss-manager.de/api/cars.php?lang=en_GB").done(data => localStorage.setItem('aVehicleTypesUk', JSON.stringify({lastUpdate: new Date().getTime(), value: data})) );
     }
 
-    var aVehicleTypesNew = JSON.parse(localStorage.aVehicleTypesNew).value;
-    var vehicleTypes = localStorage.fr_vehicleTypes ? JSON.parse(localStorage.fr_vehicleTypes) : [];
-    var aaoId = localStorage.fr_aaoId ? +localStorage.fr_aaoId : 0;
+    var aVehicleTypesUk = JSON.parse(localStorage.aVehicleTypesUk).value;
+    var vehicleTypes = localStorage.fr_vehicleTypesUk ? JSON.parse(localStorage.fr_vehicleTypesUk) : [];
+    var aaoId = localStorage.fr_aaoIdUk ? +localStorage.fr_aaoIdUk : 0;
 
     function mapVehicles(arrClasses, trigger) {
         var returnValue = [];
         if(trigger == "type") {
             returnValue = $.map(arrClasses, function(item) {
-                return aVehicleTypesNew.filter((obj) => obj.short_name == item)[0].id;
+                return aVehicleTypesUk.filter((obj) => obj.name == item)[0].id;
             });
         } else if(trigger == "name") {
             returnValue = $.map(arrClasses, function(item) {
-                return aVehicleTypesNew.filter((obj) => obj.id == item)[0].short_name;
+                return aVehicleTypesUk.filter((obj) => obj.id == item)[0].name;
             });
         }
         return returnValue;
     }
 
     if(window.location.pathname.includes("aaos") && window.location.pathname.includes("edit") && !localStorage.fr_aaoId) {
-        $("h1").append(`<a class="btn btn-info" id="frSaveAaoId" style="margin-left:2em">AAO-ID speichern</a>`);
+        $("h1").append(`<a class="btn btn-info" id="frSaveAaoId" style="margin-left:2em">save AAO-ID</a>`);
     }
 
     if(window.location.pathname.includes("missions") && localStorage.fr_aaoId) {
         var arrVehicles = [];
 
-        for(var i in aVehicleTypesNew) {
-            arrVehicles.push(aVehicleTypesNew[i].short_name);
+        for(var i in aVehicleTypesUk) {
+            arrVehicles.push(aVehicleTypesUk[i].name);
         }
         arrVehicles.sort((a, b) => a.toUpperCase() > b.toUpperCase() ? 1 : -1);
 
         $("#available_aao_" + aaoId)
             .parent()
-            .after(`<button type="button" class="btn btn-success btn-xs" data-toggle="modal" data-target="#frModal" style="height:24px">
+            .after(`<button type="button" class="btn btn-success btn-xs" data-toggle="modal" data-target="#frModalUk" style="height:24px">
                       <div class="glyphicon glyphicon-cog" style="color:LightSteelBlue"></div>
                     </button>
-                    <div class="modal fade" id="frModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal fade" id="frModalUk" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                       <div class="modal-dialog" role="document">
                         <div class="modal-content">
                           <div class="modal-header">
-                            <h5 class="modal-title" id="frModalLabel">Einstellungen</h5>
+                            <h5 class="modal-title" id="frModalLabelUk">Einstellungen</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                               <span aria-hidden="true">&times;</span>
                             </button>
                           </div>
-                          <div class="modal-body" id="frModalBody">
-                            <label for="frSelectVehicles">Fahrzeugtypen (Mehrfachauswahl mit Strg + Klick)</label>
-                            <select multiple class="form-control" id="frSelectVehicles" style="height:20em;width:20em"></select>
+                          <div class="modal-body" id="frModalBodyUk">
+                            <label for="frSelectVehicles">vehicle-types (multiple choice with Strg + click)</label>
+                            <select multiple class="form-control" id="frSelectVehiclesUk" style="height:20em;width:20em"></select>
                           </div>
                           <div class="modal-footer">
-                            <button type="button" class="btn btn-danger" data-dismiss="modal">Schließen</button>
-                            <button type="button" class="btn btn-success" id="frSavePreferences">Speichern</button>
+                            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-success" id="frSavePreferencesUk">Save</button>
                           </div>
                         </div>
                       </div>
                     </div>`);
 
         for(i in arrVehicles) {
-            $("#frSelectVehicles").append(`<option>${arrVehicles[i]}</option>`);
+            $("#frSelectVehiclesUk").append(`<option>${arrVehicles[i]}</option>`);
         }
 
-        $("#frSelectVehicles").val(mapVehicles(vehicleTypes, "name"));
+        $("#frSelectVehiclesUk").val(mapVehicles(vehicleTypes, "name"));
     }
 
-    $("body").on("click", "#frSaveAaoId", function() {
+    $("body").on("click", "#frSaveAaoIdUk", function() {
         localStorage.fr_aaoId = +window.location.pathname.replace(/\D+/g,"");
         $("#frSaveAaoId").css({"display":"none"});
         alert("AAO-ID gespeichert.");
     });
 
-    $("body").on("click", "#frSavePreferences", function() {
+    $("body").on("click", "#frSavePreferencesUk", function() {
         vehicleTypes = mapVehicles($("#frSelectVehicles").val(), "type");
-        localStorage.fr_vehicleTypes = JSON.stringify(vehicleTypes);
+        localStorage.fr_vehicleTypesUk = JSON.stringify(vehicleTypes);
 
-        $("#frModalBody").html("<h3><center>Die Einstellungen wurden gespeichert.</center></h5>");
-        $("#frSavePreferences").css({"display":"none"});
+        $("#frModalBodyUk").html("<h3><center>Settings saved successfully.</center></h5>");
+        $("#frSavePreferencesUk").css({"display":"none"});
     });
 
     $("#aao_"+aaoId).click(function() {
