b16eb18da9d99ccfa28f6950cc1e3436df84d6d1
Patrick <66879705+DrTraxx@users.noreply.github.com>
2020-12-20 08:04:13 +0100

    create alertRd

---
+++ alertRd.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         alert RD-Mitteilungen
+// @version      1.0.0
+// @description  zeigt Rückmeldungen von Einsätzen, die den RD betreffen, als Alert
+// @author       DrTraxx
+// @include      *://www.leitstellenspiel.de/missions/*
+// @grant        none
+// ==/UserScript==
+/* global $ */
+
+(function() {
+    'use strict';
+
+    if($("#mission_replies > li").length) {
+        var alertContent = "";
+
+        $("#mission_replies > li").each(function() {
+            if($(this)[0].innerText.includes("RD") || $(this)[0].innerText.includes("Rettungsdienst")) {
+                alertContent += $(this)[0].innerText + "<br>";
+            }
+        });
+
+        if(alertContent) {
+            $("#mission_general_info").parent().after(`<div class="alert alert-info">
+                                                     <button class="close" data-dismiss="alert" type="button">×</button>
+                                                     ${alertContent.trim()}
+                                                   </div>`);
+        }
+    }
+
+
+})();
