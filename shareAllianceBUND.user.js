01c56ec3af45810878186a68e0d9fae9e6679350
Patrick <66879705+DrTraxx@users.noreply.github.com>
2021-01-06 21:44:46 +0100

    Bugfix shareAllianceBUND (1.12.2)

--- shareAllianceBUND.user.js
+++ shareAllianceBUND.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ShareAllianceBUND
-// @version      1.12.1
+// @version      1.12.2
 // @description  teilt Einsätze im Verband und postet eine Rückmeldung im Chat - Dieses Script ist exklusiv für den Verband Bundesweiter KatSchutz (Bund)
 // @author       DrTraxx
 // @include      *://www.leitstellenspiel.de/missions/*
@@ -21,6 +21,10 @@
     var aMissions = await getMissions();
     var missionId = window.location.pathname.replace(/\D+/g,'');
     var missionAddress = $('#mission_general_info').children('small').text().split('|')[0].trim();
+    var config = JSON.parse(localStorage.sab_preferences);
+    var missionIdNextMission = $('#mission_next_mission_btn').attr('href').replace(/\D+/g,'');
+    var patients = $('.mission_patient').length;
+    var missionDate = $("#missionH1").attr("data-original-title").replace("Einsatz eingegangen: ","");
 
     function pushPgsl() {
         if($("#mission_replies").text()) {
@@ -40,78 +44,75 @@
         if($("#col_left .alert-info").length == 0) {
             pushPgsl();
         } else {
-            return false;
+            pushSystemMission();
         }
     }
 
-    var config = JSON.parse(localStorage.sab_preferences);
-    var missionIdNextMission = $('#mission_next_mission_btn').attr('href').replace(/\D+/g,'');
-    var missionTypeId = $('#mission_help').attr('href').split("/").pop().replace(/\?.*/, '');
-    var mission = aMissions.filter((obj) => obj.id == missionTypeId)[0];
-    var credits = mission.additional.guard_mission ? parseInt($("#col_left").text().match(/(?:Verdienst:)\s([\d.]+)/g)[0].replace(/\D+/g,'')) : mission.average_credits;
-    var patients = $('.mission_patient').length;
-    var missionDate = $("#missionH1").attr("data-original-title").replace("Einsatz eingegangen: ","");
-
     if(!$('#mission_alliance_share_btn').attr('href')) {
+        var missionTypeId = $('#mission_help').attr('href').split("/").pop().replace(/\?.*/, '');
+        var mission = aMissions.filter((obj) => obj.id == missionTypeId)[0];
+        var credits = mission.additional.guard_mission ? parseInt($("#col_left").text().match(/(?:Verdienst:)\s([\d.]+)/g)[0].replace(/\D+/g,'')) : mission.average_credits;
         if(mission.additional.only_alliance_mission === true && $("#col_left .alert-info").length == 0) {
             pushPgsl();
         } else {
-            return false;
+            pushSystemMission();
         }
     }
 
-    if(credits < 2500 && !mission.additional.guard_mission) return false;
+    function pushSystemMission() {
+        if(credits < 2500 && !mission.additional.guard_mission) return false;
 
-    $('#mission_finish_now_btn')
-        .parent()
-        .after(`<div class="btn-group dropup">
-                  <a class="btn btn-success btn-sm" id="shareBund" title="Alarmieren, im Verband freigeben und eine Rückmeldung mit der Adresse in den Chat senden." style="height:32px">
-                    <img class="icon icons8-Phone-Filled" src="/images/icons8-phone_filled.svg" width="16" height="16">
-                    <img class="icon icons8-Share" src="/images/icons8-share.svg" width="16" height="16">
-                    <span class="glyphicon glyphicon-info-sign"></span>
-                    <span class="glyphicon glyphicon-arrow-right" id="jumpArrow" style="display:${config.jumpNext ? `inline` : `none`}"></span>
-                 </a>
-                  <button type="button" class="btn btn-success btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="height:32px" id="btnSabOptions">
-                    <div class="glyphicon glyphicon-cog" style="color:LightSteelBlue"></div>
-                  </button>
-                  <div class="dropdown-menu">
-                    <div class="dropdown-item form-check">
-                      <input type="checkbox" class="form-check-input" id="cbxJumpNext" ${config.jumpNext ? `checked`: ``}>
-                      <label class="form-check-label" for="cbxJumpNext" title="zum nächsten Einsatz springen">nächster Einsatz</label>
-                    </div>
-                    <div class="dropdown-item form-check">
-                      <input type="checkbox" class="form-check-input" id="cbxShowCredits" ${config.showCredits ? `checked`: ``}>
-                      <label class="form-check-label" for="cbxShowCredits" title="Durchschn. Verdienst in die Rückmeldung schreiben">zeige Verdienst</label>
-                    </div>
-                    <div class="dropdown-item form-check">
-                      <input type="checkbox" class="form-check-input" id="cbxPushPatients" ${config.pushPatients ? `checked`: ``}>
-                      <label class="form-check-label" for="cbxPushPatients" title="Anzahl Patienten an EST in die Rückmeldung schreiben">zeige Patienten</label>
-                    </div>
-                    <div class="dropdown-item form-check">
-                      <input type="checkbox" class="form-check-input" id="cbxDate" ${config.showDate ? `checked`: ``}>
-                      <label class="form-check-label" for="cbxOptionalText" title="Einsatzdatum in die Rückmeldung schreiben">zeige Einsatzdatum</label>
-                    </div>
-                    <div class="dropdown-item form-check">
-                      <input type="checkbox" class="form-check-input" id="cbxAllianceChat" ${config.allianceChat ? `checked`: ``}>
-                      <label class="form-check-label" for="cbxAllianceChat" title="Rückmeldung in den Chat senden">im Verbandchat pushen</label>
-                    </div>
-                    <div class="dropdown-item form-check">
-                      <input type="checkbox" class="form-check-input" id="cbxOptionalText" ${config.optionalText.bol ? `checked`: ``}>
-                      <label class="form-check-label" for="cbxOptionalText" title="zusätzliche Rückmeldung abgeben. (z.B. dringend benötigte Fahrzeuge)">zus. Rückmeldung</label>
-                    </div>
-                    <div class="dropdown-item input-group">
-                      <input type="text" class="form-control form-control-sm" value="${config.optionalText.value}" placeholder="zusätzliche Rückmeldung" id="iptConfigOptionalText" title="zusätzliche Rückmeldung" style="width:20em;height:22px">
-                    </div>
-                    <div class="dropdown-item input-group btn-group">
-                      <input type="text" class="form-control form-control-sm" value="${config.shortKey}" id="iptShortKey" title="Strg (Mac: control) + Shift + key" style="width:4em;height:22px">
-                      <a class="btn btn-info btn-xs" href="https://keycode.info/" target="_blank" title="Short-Key suchen">Short-Key</a>
-                    </div>
-                    <div class="dropdown-item btn-group">
-                      <a class="btn btn-success btn-xs" id="sabSavePreferences">Speichern</a>
-                    </div>
-                  </div>
-                  <input class="form-control form-control-sm" type="text" placeholder="zusätzliche Rückmeldung" value="${config.optionalText.value ? config.optionalText.value : ``}" id="iptOptionalText" style="height:32px;width:20em;display:${config.optionalText.bol ? `inherit` : `none`}">
-                </div>`);
+        $('#mission_finish_now_btn')
+            .parent()
+            .after(`<div class="btn-group dropup">
+                     <a class="btn btn-success btn-sm" id="shareBund" title="Alarmieren, im Verband freigeben und eine Rückmeldung mit der Adresse in den Chat senden." style="height:32px">
+                       <img class="icon icons8-Phone-Filled" src="/images/icons8-phone_filled.svg" width="16" height="16">
+                       <img class="icon icons8-Share" src="/images/icons8-share.svg" width="16" height="16">
+                       <span class="glyphicon glyphicon-info-sign"></span>
+                       <span class="glyphicon glyphicon-arrow-right" id="jumpArrow" style="display:${config.jumpNext ? `inline` : `none`}"></span>
+                    </a>
+                     <button type="button" class="btn btn-success btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="height:32px" id="btnSabOptions">
+                       <div class="glyphicon glyphicon-cog" style="color:LightSteelBlue"></div>
+                     </button>
+                     <div class="dropdown-menu">
+                       <div class="dropdown-item form-check">
+                         <input type="checkbox" class="form-check-input" id="cbxJumpNext" ${config.jumpNext ? `checked`: ``}>
+                         <label class="form-check-label" for="cbxJumpNext" title="zum nächsten Einsatz springen">nächster Einsatz</label>
+                       </div>
+                       <div class="dropdown-item form-check">
+                         <input type="checkbox" class="form-check-input" id="cbxShowCredits" ${config.showCredits ? `checked`: ``}>
+                         <label class="form-check-label" for="cbxShowCredits" title="Durchschn. Verdienst in die Rückmeldung schreiben">zeige Verdienst</label>
+                       </div>
+                       <div class="dropdown-item form-check">
+                         <input type="checkbox" class="form-check-input" id="cbxPushPatients" ${config.pushPatients ? `checked`: ``}>
+                         <label class="form-check-label" for="cbxPushPatients" title="Anzahl Patienten an EST in die Rückmeldung schreiben">zeige Patienten</label>
+                       </div>
+                       <div class="dropdown-item form-check">
+                         <input type="checkbox" class="form-check-input" id="cbxDate" ${config.showDate ? `checked`: ``}>
+                         <label class="form-check-label" for="cbxOptionalText" title="Einsatzdatum in die Rückmeldung schreiben">zeige Einsatzdatum</label>
+                       </div>
+                       <div class="dropdown-item form-check">
+                         <input type="checkbox" class="form-check-input" id="cbxAllianceChat" ${config.allianceChat ? `checked`: ``}>
+                         <label class="form-check-label" for="cbxAllianceChat" title="Rückmeldung in den Chat senden">im Verbandchat pushen</label>
+                       </div>
+                       <div class="dropdown-item form-check">
+                         <input type="checkbox" class="form-check-input" id="cbxOptionalText" ${config.optionalText.bol ? `checked`: ``}>
+                         <label class="form-check-label" for="cbxOptionalText" title="zusätzliche Rückmeldung abgeben. (z.B. dringend benötigte Fahrzeuge)">zus. Rückmeldung</label>
+                       </div>
+                       <div class="dropdown-item input-group">
+                         <input type="text" class="form-control form-control-sm" value="${config.optionalText.value}" placeholder="zusätzliche Rückmeldung" id="iptConfigOptionalText" title="zusätzliche Rückmeldung" style="width:20em;height:22px">
+                       </div>
+                       <div class="dropdown-item input-group btn-group">
+                         <input type="text" class="form-control form-control-sm" value="${config.shortKey}" id="iptShortKey" title="Strg (Mac: control) + Shift + key" style="width:4em;height:22px">
+                         <a class="btn btn-info btn-xs" href="https://keycode.info/" target="_blank" title="Short-Key suchen">Short-Key</a>
+                       </div>
+                       <div class="dropdown-item btn-group">
+                         <a class="btn btn-success btn-xs" id="sabSavePreferences">Speichern</a>
+                       </div>
+                     </div>
+                     <input class="form-control form-control-sm" type="text" placeholder="zusätzliche Rückmeldung" value="${config.optionalText.value ? config.optionalText.value : ``}" id="iptOptionalText" style="height:32px;width:20em;display:${config.optionalText.bol ? `inherit` : `none`}">
+                   </div>`);
+    }
 
     async function alarmAndShare(){
 
