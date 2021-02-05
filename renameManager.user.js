c3f723f10db01243e188a39b9b6e2fcc87e21724
Patrick <66879705+DrTraxx@users.noreply.github.com>
2021-02-05 16:01:53 +0100

    bugfix renameManager (0.1.2 - open BETA)

--- renameManager.user.js
+++ renameManager.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         renameManager
-// @version      0.1.1 - open BETA
+// @version      0.1.2 - open BETA
 // @description  Fahrzeuge umbenennen
 // @author       DrTraxx
 // @include      /^https?:\/\/(?:w{3}\.)?(?:(policie\.)?operacni-stredisko\.cz|(politi\.)?alarmcentral-spil\.dk|(polizei\.)?leitstellenspiel\.de|missionchief\.gr|(?:(police\.)?missionchief-australia|(police\.)?missionchief|(poliisi\.)?hatakeskuspeli|missionchief-japan|missionchief-korea|nodsentralspillet|meldkamerspel|operador193|jogo-operador112|jocdispecerat112|dispecerske-centrum|112-merkez|dyspetcher101-game)\.com|(police\.)?missionchief\.co\.uk|centro-de-mando\.es|centro-de-mando\.mx|(police\.)?operateur112\.fr|(polizia\.)?operatore112\.it|operatorratunkowy\.pl|dispetcher112\.ru|larmcentralen-spelet\.se)\/.*$/
@@ -115,6 +115,7 @@ overflow-y: auto;
     }
 
     if(!config.vehicle_types) {
+        config.vehicle_types = {};
         for(var a in aVehicleTypesNew) {
             var c = aVehicleTypesNew[i];
             config.vehicle_types[c.id].alias_one = c.short_name;
