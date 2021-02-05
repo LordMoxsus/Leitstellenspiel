179464c1586180e45d6a9b5a8d4869e227ac4e73
Patrick <66879705+DrTraxx@users.noreply.github.com>
2021-02-05 15:53:34 +0100

    update renameManager (0.1.1 - open BETA)

--- renameManager.user.js
+++ renameManager.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         renameManager
-// @version      0.1.0 - open BETA
+// @version      0.1.1 - open BETA
 // @description  Fahrzeuge umbenennen
 // @author       DrTraxx
 // @include      /^https?:\/\/(?:w{3}\.)?(?:(policie\.)?operacni-stredisko\.cz|(politi\.)?alarmcentral-spil\.dk|(polizei\.)?leitstellenspiel\.de|missionchief\.gr|(?:(police\.)?missionchief-australia|(police\.)?missionchief|(poliisi\.)?hatakeskuspeli|missionchief-japan|missionchief-korea|nodsentralspillet|meldkamerspel|operador193|jogo-operador112|jocdispecerat112|dispecerske-centrum|112-merkez|dyspetcher101-game)\.com|(police\.)?missionchief\.co\.uk|centro-de-mando\.es|centro-de-mando\.mx|(police\.)?operateur112\.fr|(polizia\.)?operatore112\.it|operatorratunkowy\.pl|dispetcher112\.ru|larmcentralen-spelet\.se)\/.*$/
@@ -112,6 +112,15 @@ overflow-y: auto;
         console.debug("config beim Speichern", config);
         var databaseContent = personalNotes + "\n\n\n\n\n" + databaseStart + "\n" + (databases.includes(placeholderDatabase) ? databases.replace(renameRegex, placeholderDatabase + "\n" + JSON.stringify(config)) : (databases ? (databases + "\n" + placeholderDatabase + "\n" + JSON.stringify(config)) : (placeholderDatabase + "\n" + JSON.stringify(config))));
         await $.post("/note", {"note": {"message": databaseContent}, "authenticity_token" : $("meta[name=csrf-token]").attr("content"), "_method": "put"});
+    }
+
+    if(!config.vehicle_types) {
+        for(var a in aVehicleTypesNew) {
+            var c = aVehicleTypesNew[i];
+            config.vehicle_types[c.id].alias_one = c.short_name;
+            config.vehicle_types[c.id].alias_two = c.name;
+        }
+        await saveInNotes();
     }
 
     async function saveToLocalStorage(type) {
