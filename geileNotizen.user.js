9ddf593f98460d581151be987554ffe40a98fb38
Patrick <66879705+DrTraxx@users.noreply.github.com>
2021-02-15 00:22:35 +0100

    Bugfix geileNotizen (1.0.1)

--- geileNotizen.user.js
+++ geileNotizen.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         GeileNotizen
-// @version      1.0.0
+// @version      1.0.1
 // @description  ermöglicht Notizen unterhalb der Einsatzliste
 // @author       DrTraxx
 // @include      /^https?:\/\/(?:w{3}\.)?(?:(policie\.)?operacni-stredisko\.cz|(politi\.)?alarmcentral-spil\.dk|(polizei\.)?leitstellenspiel\.de|missionchief\.gr|(?:(police\.)?missionchief-australia|(police\.)?missionchief|(poliisi\.)?hatakeskuspeli|missionchief-japan|missionchief-korea|nodsentralspillet|meldkamerspel|operador193|jogo-operador112|jocdispecerat112|dispecerske-centrum|112-merkez|dyspetcher101-game)\.com|(police\.)?missionchief\.co\.uk|centro-de-mando\.es|centro-de-mando\.mx|(police\.)?operateur112\.fr|(polizia\.)?operatore112\.it|operatorratunkowy\.pl|dispetcher112\.ru|larmcentralen-spelet\.se)\/.*$/
@@ -17,7 +17,7 @@
         .parent()
         .parent()
         .after(`<div class="form-group btn-group">
-                  <label for="geileNotizenTextarea">Example textarea</label>
+                  <label for="geileNotizenTextarea">Geile Notizen</label>
                   <textarea class="form-control" id="geileNotizenTextarea" rows="4" value="${localStorage.geile_notizen}" style="width:40em"></textarea>
                   <a class="btn btn-success btn-xs" id="geileNotizenSave">Speichern</a>
                   <a class="btn btn-danger btn-xs" id="geileNotizenClear">Löschen</a>
